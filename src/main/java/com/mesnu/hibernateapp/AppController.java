2f9dbe9f567ab05ece5e251eb30f7a3fbc0d3e6e
mesnuuu <mesnuuu@gmail.com>
2021-03-23 08:11:27 +0500

    9. Implement Create Product Feature
    You can see in the index.html, we have a hyperlink
    
    <a href="/new">Create New Product</a>
    
    ->  handled by the method -
     showNewProductPagemethod ( )
    
    -> For the view, create the new_product.html
    
    -> And we need to code another handler method to save the product information into the database:
    
    saveProduct( )

--- src/main/java/com/mesnu/hibernateapp/AppController.java
+++ src/main/java/com/mesnu/hibernateapp/AppController.java
@@ -7,7 +7,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 
@@ -25,5 +27,20 @@ public class AppController {
         model.addAttribute("listProducts", listProducts);
          
         return "index";
+    }
+    
+    @RequestMapping("/new")
+    public String showNewProductPage(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
+         
+        return "new_product";
+    }
+    
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveProduct(@ModelAttribute("product") Product product) {
+        service.save(product);
+         
+        return "redirect:/";
     }
 }
