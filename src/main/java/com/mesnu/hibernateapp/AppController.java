11c12d0f4a660201da2dd99855f812ba3825d55b
mesnuuu <mesnuuu@gmail.com>
2021-03-23 08:23:06 +0500

    10. Implement Edit Product Feature
    In the home page - hyperlink that allows the users to edit a product:
    
    <a th:href="/@{'/edit/' + ${product.id}}">Edit</a>
    
    -> Code the handler method  showEditProductPage()
    
    -> Study ModelAndView in Spring
    
    -> code the view page edit_product.html
    
    -> Test Edit page -
    
    *Click the Save button will update the product information into the database.
    
    *The handler method saveProduct() is reused in this case.

--- src/main/java/com/mesnu/hibernateapp/AppController.java
+++ src/main/java/com/mesnu/hibernateapp/AppController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 
 
@@ -42,5 +44,14 @@ public class AppController {
         service.save(product);
          
         return "redirect:/";
+    }
+    
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditProductPage(@PathVariable(name = "id") int id) {
+        ModelAndView mav = new ModelAndView("edit_product");
+        Product product = service.get(id);
+        mav.addObject("product", product);
+         
+        return mav;
     }
 }
